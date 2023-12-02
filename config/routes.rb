745728d39ca50a81b4be7f1084c13feda029e2fa
DevaWinner <winneraaniekan@gmail.com>
2023-12-02 12:25:10 +0100

    Add initial configuration files for Rails application

---
+++ config/routes.rb
@@ -0,0 +1,13 @@
+Rails.application.routes.draw do
+  devise_for :users, path: 'users',
+  path_names: { sign_in: 'sign_in', sign_out: 'sign_out', password: 'secret', registration: 'register', sign_up: 'sign_up' }
+
+  devise_scope :user do
+    get '/users/sign_out', to: 'devise/sessions#destroy'
+    root to: 'groups#index'
+  end
+  
+  resources :users
+  resources :groups, only: [:index, :new, :create]
+  resources :expenses, only: [:index, :new, :create]
+end
